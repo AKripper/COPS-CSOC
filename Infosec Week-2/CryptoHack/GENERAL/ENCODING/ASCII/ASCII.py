05b0e1f4465b6b77b204d16d7dafc5db45557dc4
AKripper <167231621+AKripper@users.noreply.github.com>
2024-06-21 20:01:29 +0530

    Update ASCII.py

--- Infosec Week-2/CryptoHack/GENERAL/ENCODING/ASCII/ASCII.py
+++ Infosec Week-2/CryptoHack/GENERAL/ENCODING/ASCII/ASCII.py
@@ -1,4 +1,7 @@
 flag=[99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
 
+# converts ddecimal to ascii
 for i in flag:
     print(chr(i),end="")
+
+# Output : crypto{ASCII_pr1nt4bl3}

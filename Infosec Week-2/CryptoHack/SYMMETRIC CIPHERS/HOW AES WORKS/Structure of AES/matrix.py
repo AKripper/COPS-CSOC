b1fc80f3ce268796092a4ec387b8fec7d7844440
AKripper <167231621+AKripper@users.noreply.github.com>
2024-06-21 20:54:20 +0530

    Update matrix.py

--- Infosec Week-2/CryptoHack/SYMMETRIC CIPHERS/HOW AES WORKS/Structure of AES/matrix.py
+++ Infosec Week-2/CryptoHack/SYMMETRIC CIPHERS/HOW AES WORKS/Structure of AES/matrix.py
@@ -21,3 +21,9 @@ print(matrix2bytes(matrix))
 print("Flag: ",end="")
 for i in matrix2bytes(matrix):
     print(chr(i),end="")
+
+'''
+Output:
+[99, 114, 121, 112, 116, 111, 123, 105, 110, 109, 97, 116, 114, 105, 120, 125]
+Flag: crypto{inmatrix}
+'''

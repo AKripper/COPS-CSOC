efb354fa5deaf0222cfad5ab00b51f333f8441b8
AKripper <167231621+AKripper@users.noreply.github.com>
2024-06-21 20:04:14 +0530

    Update B2L.py

--- Infosec Week-2/CryptoHack/GENERAL/ENCODING/Bytes and Big Integers/B2L.py
+++ Infosec Week-2/CryptoHack/GENERAL/ENCODING/Bytes and Big Integers/B2L.py
@@ -1,5 +1,7 @@
 from Crypto.Util.number import *
 
 num=11515195063862318899931685488813747395775516287289682636499965282714637259206269
-flag=long_to_bytes(num)
+flag=long_to_bytes(num)  # Converts decimal to bytes
 print(flag)
+
+# Output : b'crypto{3nc0d1n6_4ll_7h3_w4y_d0wn}'

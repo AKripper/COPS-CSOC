a8432a5843ce384df57620d6f610dc2c3d4bf0e2
AKripper <167231621+AKripper@users.noreply.github.com>
2024-06-21 20:44:08 +0530

    Update script.py

--- Infosec Week-2/CryptoHack/GENERAL/XOR/You either know, XOR you don't/script.py
+++ Infosec Week-2/CryptoHack/GENERAL/XOR/You either know, XOR you don't/script.py
@@ -1,7 +1,12 @@
 from pwn import *
 
 str="0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104"
-byte=bytes.fromhex(str)
-key="myXORkey"
+
+byte=bytes.fromhex(str)  # Converts hexadecimal to bytes
+key="myXORkey"           # Key we found out using trial andd error
+
+# Prints the flag
 flag=xor(byte,key)
 print(flag)
+
+# Output : b'crypto{1f_y0u_Kn0w_En0uGH_y0u_Kn0w_1t_4ll}'

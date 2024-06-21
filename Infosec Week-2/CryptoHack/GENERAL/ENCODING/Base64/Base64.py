3b683fe29f07fe3c7cc9c71474c853e90bd62e9a
AKripper <167231621+AKripper@users.noreply.github.com>
2024-06-21 20:02:59 +0530

    Update Base64.py

--- Infosec Week-2/CryptoHack/GENERAL/ENCODING/Base64/Base64.py
+++ Infosec Week-2/CryptoHack/GENERAL/ENCODING/Base64/Base64.py
@@ -1,5 +1,7 @@
 import base64
 
 hex="72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
-flag=bytes.fromhex(hex)
-print(base64.b64encode(flag))
+flag=bytes.fromhex(hex)        # converts hex to bytes
+print(base64.b64encode(flag))  # base64 encodes the flag
+
+# Output : b'crypto/Base+64+Encoding+is+Web+Safe/'

f25bc39fab5d5d48823bf22af09a56213aa1a19d
AKripper <167231621+AKripper@users.noreply.github.com>
2024-06-21 20:45:32 +0530

    Update great_snakes_35381fca29d68d8f3f25c9fa0a9026fb.py

--- Infosec Week-2/CryptoHack/INTRODUCTION/Great Snakes/great_snakes_35381fca29d68d8f3f25c9fa0a9026fb.py
+++ Infosec Week-2/CryptoHack/INTRODUCTION/Great Snakes/great_snakes_35381fca29d68d8f3f25c9fa0a9026fb.py
@@ -10,3 +10,9 @@ ords = [81, 64, 75, 66, 70, 93, 73, 72, 1, 92, 109, 2, 84, 109, 66, 75, 70, 90, 
 
 print("Here is your flag:")
 print("".join(chr(o ^ 0x32) for o in ords))
+
+'''
+Output : 
+Here is your flag:
+crypto{z3n_0f_pyth0n}
+'''

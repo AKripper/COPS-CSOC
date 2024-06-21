a015c46637ab96b3c65384f05e2984e9615294fd
AKripper <167231621+AKripper@users.noreply.github.com>
2024-06-21 19:56:00 +0530

    Update source.py

--- Infosec Week-2/Challenge 1/source.py
+++ Infosec Week-2/Challenge 1/source.py
@@ -7,7 +7,7 @@ s = ''.join(format(ord(i), '02x') for i in flag)
 e = ""
 
 for i in range(0,len(s),4):
-    # XORS the first two digits with the first four digits and adds the four digit answer to the sncoded string.
+    # XORS the first two digits with the first four digits and adds the four digit answer to the encoded string.
     e += format(int(s[i:i+2],16)^int(s[i:i+4],16), '02x')
 
 # Outputs the encoded string.
